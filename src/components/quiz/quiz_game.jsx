734ac5b57b6a388fdda5420a8132403f8b5aaaee
saju-sajadh <sajadhtechosa@gmail.com>
2025-12-16 12:08:56 +0530

    Update quiz_game.jsx

--- src/components/quiz/quiz_game.jsx
+++ src/components/quiz/quiz_game.jsx
@@ -28,7 +28,7 @@ export default function QuizGame({ topic, grade, onClose }) {
     setLoading(false);
   }
   const genAI = new GoogleGenerativeAI(apiKey);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
   useEffect(() => {
     const fetchQuestions = async () => {
